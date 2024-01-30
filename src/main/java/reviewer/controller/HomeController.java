a7e6b12d558d5677ba06c3ac3900ad0f5adb5d00
tbharath03 <topajibharath07@gmail.com>
2024-01-30 23:30:00 +0530

    removed errors and added controllers and html templates for home,login,register

--- src/main/java/reviewer/controller/HomeController.java
+++ src/main/java/reviewer/controller/HomeController.java
@@ -1,11 +1,13 @@
-package reviewer;
+package reviewer.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
 @Controller
 public class HomeController {
-
-  @GetMapping("/registerpage.html")
-  public String home() {
-    return "home";
-  }
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
 }
