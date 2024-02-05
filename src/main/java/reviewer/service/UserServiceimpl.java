94c47ac37510c5566ff389a4b8cdf4db4b690f01
attanti123 <153095982+attanti123@users.noreply.github.com>
2024-02-05 21:53:53 +0530

    added api  to UserServiceimpl class

--- src/main/java/reviewer/service/UserServiceimpl.java
+++ src/main/java/reviewer/service/UserServiceimpl.java
@@ -7,24 +7,40 @@ import reviewer.model.Review;
 import reviewer.model.User;
 import reviewer.repository.ReviewRepository;
 import reviewer.repository.UserRepository;
-
+/**
+ * Service implementation for user-related operations.
+ */
 @Service
 public class UserServiceimpl implements UserService
 {
 	@Autowired
 	private UserRepository repo;
 	private ReviewRepository reviewRepository;
+	/**
+     * Constructor to inject dependencies.
+     *
+     * @param reviewRepository The repository for reviews.
+     */
     public UserServiceimpl(ReviewRepository reviewRepository) {
         this.reviewRepository = reviewRepository;
     }
 
-	
+    /**
+     * Registers a new user.
+     *
+     * @param user The user to register.
+     */
+
 	@Override
 	public void registerUser(User user) {
 	
 		repo.save(user);
 	}
-
+	/**
+     * Submits a review by the user.
+     *
+     * @param review The review to be submitted.
+     */
 	@Override
 	public void ReviewbyUser(Review review) 
 	{
