05b873635cc408fb9f528da6526efcf1b67e5f90
attanti123 <153095982+attanti123@users.noreply.github.com>
2024-02-05 21:55:27 +0530

    added  api to  LoginController class

--- src/main/java/reviewer/controller/LoginController.java
+++ src/main/java/reviewer/controller/LoginController.java
@@ -1,29 +1,50 @@
 package reviewer.controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import reviewer.model.User;
 
+/**
+ * Controller class for handling user login.
+ *
+ * This controller manages user login functionality, including password hashing for security.
+ * The password provided during login will be hashed, and the hash value will be verified against
+ * the stored hash in the database for authentication.
+ *
+ * @author Attanti
+ * @version 1.0
+ * @since 2024-02-05
+ */
 @Controller
-@RequestMapping("/login")
-/*
- * This is the controller for LoginPage
-*/
+@RequestMapping("/loginPage")
 public class LoginController {
 
+	/**
+     * Handles HTTP GET requests for the "/loginPage" path.
+     *
+     * This method prepares a new User object and adds it to the model.
+     * The User object is then used to capture user credentials during the login process.
+     *
+     * @param model The model to add attributes to the model
+     * @return The logical view name "loginPage"
+     *
+     *
+     *
+     * See Also:
+     *   - {@link User} For information about the User model.
+     *   - /loginPage The login page endpoint.
+     *    
+     * @author Attanti
+     * @version 1.0
+     * @since 2024-02-05
+     */
 	@GetMapping
-	public String login(Model model) {
+	public String login(Model model) 
+	{
 		User user = new User();
 		model.addAttribute("user",user);
-		return "login";
+		return "loginPage";
 	}
 	
-	@PostMapping
-	public String login1(@ModelAttribute User user) {
-		//System.out.println("hello");
-		return "redirect:/home";
-	}
 }
