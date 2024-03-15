4c18f36a188e24de39ef9e85d6ce29a90fb9860d
tbharath03 <topajibharath07@gmail.com>
2024-03-15 14:54:36 +0530

    done with already reviewed,need to review and dashboard

--- src/main/java/reviewer/controller/ReviewController.java
+++ src/main/java/reviewer/controller/ReviewController.java
@@ -2,7 +2,6 @@ package reviewer.controller;
 
 import java.security.Principal;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +10,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import reviewer.model.Paper;
 import reviewer.model.Review;
 import reviewer.model.User;
 import reviewer.repository.PaperRepository;
 import reviewer.repository.ReviewRepository;
 import reviewer.repository.UserRepository;
 import reviewer.service.UserService;
+import reviewer.util.Rkey;
 /**
  * Controller class for handling reviews.
  *
@@ -37,8 +38,6 @@ public class ReviewController {
         this.paperRepo = paperRepo;
         this.reviewRepo = reviewRepo;
     }
-	@Autowired
-	private UserService service;
 	 /**
      * Displays the review page.
      *
@@ -49,12 +48,24 @@ public class ReviewController {
      */
 
 	@GetMapping("/review")
-	public String reviewpage(Model model) 
+	public String reviewpage(Model model,Principal principal,@RequestParam("id") Long paperId) 
 	{
-		Review review=new Review();
+//		Review review=new Review();
+//		System.out.println(review);
+//		model.addAttribute("review",review);
+//		System.out.println(review);
+		String username = principal.getName(); // Get the username of the currently logged-in user
+        User user = repo.findByEmailId(username);
+        model.addAttribute("user",user);
+        
+        model.addAttribute("paper",paperRepo.findBypid(paperId));
+		
+			Review review = reviewRepo.findById(new Rkey(paperId,username)).get();
+		    model.addAttribute("review",review);
+		
+		System.out.println(paperId);
 		System.out.println(review);
-		model.addAttribute("review",review);
-		System.out.println(review);
+		
 		return "review";
 	}
 	 /**
@@ -67,11 +78,21 @@ public class ReviewController {
      * @see UserService For information about the UserService used for review processing.
      */
 	@PostMapping("/review")
-	public String review(@ModelAttribute Review review,Principal principal)
+	public String review(@ModelAttribute Review review,Principal principal,
+			@RequestParam("id") Long paperId)
 	{
+		
 		String username = principal.getName(); // Get the username of the currently logged-in user
         User user = repo.findByEmailId(username);
-		service.ReviewbyUser(review);
-		return "dashboard";
+        review.setRid(new Rkey(paperId,username));
+        
+//		review.setUser(user);
+		Paper paper = paperRepo.findBypid(paperId);
+		review.setStatus("Already reviewed");
+//		review.setPaper(paper);
+		System.out.println(review);
+		reviewRepo.save(review);
+		
+		return "redirect:/dashboard";
 	}
 }
