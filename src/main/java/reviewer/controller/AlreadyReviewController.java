32d7a5af022a140056d9a42890b71622b02efe0a
tbharath03 <topajibharath07@gmail.com>
2024-03-15 10:09:39 +0530

    changed alreadyReviewedController class to retrieve paper data

--- src/main/java/reviewer/controller/AlreadyReviewController.java
+++ src/main/java/reviewer/controller/AlreadyReviewController.java
@@ -1,14 +1,18 @@
 package reviewer.controller;
 
 import java.security.Principal;
+import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import reviewer.model.Paper;
+import reviewer.model.Review;
 import reviewer.model.User;
 import reviewer.repository.PaperRepository;
+import reviewer.repository.ReviewRepository;
 import reviewer.repository.UserRepository;
 
 /**
@@ -19,9 +23,11 @@ import reviewer.repository.UserRepository;
 public class AlreadyReviewController {
 	private UserRepository repo;
 	private PaperRepository paperRepo;
-	public AlreadyReviewController(UserRepository repo,PaperRepository paperRepo) {
+	private ReviewRepository reviewRepo;
+	public AlreadyReviewController(UserRepository repo,PaperRepository paperRepo,ReviewRepository reviewRepo) {
         this.repo = repo;
         this.paperRepo = paperRepo;
+        this.reviewRepo = reviewRepo;
     }
 	/**
      * Handles HTTP GET requests for the already reviewed page.
@@ -34,6 +40,20 @@ public class AlreadyReviewController {
         User user = repo.findByEmailId(username); // Retrieve the user details from the repository using the username
         model.addAttribute("user", user); // Add the user details to the model
         model.addAttribute("paperCount", paperRepo.count());
+        
+		ArrayList<Review> reviews = reviewRepo.findAllByuserAndStatus(user,"Already Reviewed");
+        ArrayList<Paper> papers = new ArrayList<Paper>();
+        ArrayList<Paper> pp = new ArrayList<Paper>();
+        reviews.forEach((rev)->{papers.add(rev.getPaper());});
+        papers.forEach((p)->{
+        	pp.add(paperRepo.findBypid(p.getPid()));
+        	System.out.println(p.getPid());
+        });
+        
+        System.out.println(pp);
+        
+        model.addAttribute("papers",pp);
+        
         return "AlreadyReviewed";
     }
 }
